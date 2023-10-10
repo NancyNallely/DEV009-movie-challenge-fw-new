5df51f4d27932ef4a2e2342dd9e053ef22a2d50f
Nancy Nallely Martinez Dominguez <nancymartinezdominguez11@gmail.com>
2023-10-10 17:14:36 -0600

    Se agrega componente MovieLogo

---
+++ Movie-Challenge-App/src/components/MovieLogo.jsx
@@ -0,0 +1,11 @@
+// Logo.js
+const MovieLogo = () => {
+    return (
+      <div className="logo">
+        <img src="src/imagenes/adiccion1.png" alt="Logo de la aplicación" style={{ width: '100%' }} />
+      </div>
+    );
+  };
+  
+  export default MovieLogo;
+  
