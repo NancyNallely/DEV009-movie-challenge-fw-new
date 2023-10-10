ba1e4e7031f9467d3b114e306ee3d7fa22fa326a
Nancy Nallely Martinez Dominguez <nancymartinezdominguez11@gmail.com>
2023-10-10 17:15:40 -0600

    Se agrega componente MoviePagination

---
+++ Movie-Challenge-App/src/components/MoviePagination.jsx
@@ -0,0 +1,37 @@
+
+import PropTypes from 'prop-types';
+
+const Pagination = ({ currentPage, totalPages, onPageChange }) => {
+  // Deshabilitar el botón "Anterior" si currentPage es 1
+  const isPreviousDisabled = currentPage === 1;
+
+  // Deshabilitar el botón "Siguiente" si currentPage es igual a totalPages
+  const isNextDisabled = currentPage === totalPages;
+
+  return (
+    <div className="pagination">
+      <button
+        onClick={() => onPageChange(currentPage - 1)}
+        disabled={isPreviousDisabled}
+      >
+        Anterior
+      </button>
+      <span>Página {currentPage} de {totalPages}</span>
+      <button
+        onClick={() => onPageChange(currentPage + 1)}
+        disabled={isNextDisabled}
+      >
+        Siguiente
+      </button>
+    </div>
+  );
+};
+
+// Validación de propiedades (PropTypes)
+Pagination.propTypes = {
+  currentPage: PropTypes.number.isRequired,   // currentPage debe ser un número y es requerido
+  totalPages: PropTypes.number.isRequired,    // totalPages debe ser un número y es requerido
+  onPageChange: PropTypes.func.isRequired,    // onPageChange debe ser una función y es requerida
+};
+
+export default Pagination;
