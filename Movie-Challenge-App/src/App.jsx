b1ad06c8a2cbc748753e173c279065a5c216309e
Nancy Nallely Martinez Dominguez <nancymartinezdominguez11@gmail.com>
2023-10-11 19:17:00 -0600

    Se agrego el routeo

--- Movie-Challenge-App/src/App.jsx
+++ Movie-Challenge-App/src/App.jsx
@@ -1,11 +1,16 @@
-
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MovieCatalog from './components/MovieCatalog';
 
 function App() {
   return (
     <div className="App">
-      <MovieCatalog />
+      <Router>
+        <Routes>
+          <Route path="/" element={<MovieCatalog />} />
+          {/* Add more routes here if necessary */}
+        </Routes>
+      </Router>
     </div>
   );
 }
