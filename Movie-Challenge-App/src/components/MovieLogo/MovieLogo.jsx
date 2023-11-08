5d554d1d97aa3421d7dcfa660d792747cc66ee06
Nancy Nallely Martinez Dominguez <nancymartinezdominguez11@gmail.com>
2023-11-08 13:40:43 -0600

    Actualizacion en cambios de test hu3

--- Movie-Challenge-App/src/components/MovieLogo/MovieLogo.jsx
+++ Movie-Challenge-App/src/components/MovieLogo/MovieLogo.jsx
@@ -2,7 +2,7 @@
 const MovieLogo = () => {
     return (
       <div className="logo">
-        <img src="src/imagenes/adiccion1.png" alt="Logo de la aplicación" style={{ width: '100%' }} />
+        <img src="src/imagenes/adiccion1-min-new.jpg" alt="Logo de la aplicación" style={{ width: '100%' }} />
       </div>
     );
   };
